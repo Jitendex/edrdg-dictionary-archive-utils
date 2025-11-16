aee6725cb39712816a3441aac3bd713db6b8db81
stephenmk <stephenmk@users.noreply.github.com>
2025-11-16 10:10:13 -0600

    Use lowercase org name

--- edrdg_dictionary_archive.fish
+++ edrdg_dictionary_archive.fish
@@ -16,8 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set VERSION   '2025.11.15.0'
-set ORG_NAME  'Jitendex'
+set VERSION   '2025.11.16.0'
+set ORG_NAME  'jitendex'
 set PROJ_NAME 'edrdg-dictionary-archive'
 
 set FILENAMES 'JMdict' \
